ff8f6f232d7824312fb2e8e07d7f019727f27720
Johana <araya.johana94@gmail.com>
2023-10-29 13:31:15 -0300

    creacion del proyecto, agrego masterpage, default, bootstrap, dominio y negocio y algunas clases

---
+++ TPC_Equipo-5/App_Gestion_Turnos/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="App_Gestion_Turnos.Global" Language="C#" %>
---
+++ TPC_Equipo-5/Dominio/Paciente.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Paciente
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public DateTime Fecha_Nacimiento { get; set; }
+        public string Genero { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+        public string Mail { get; set; }
+        public string Observaciones { get; set; }
+    }
+}
